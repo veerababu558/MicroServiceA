3423e72e1d365a76854489affdf8164c050fe01d
veerababu558 <44125493+veerababu558@users.noreply.github.com>
2025-01-25 23:12:07 -0500

    Update FeignClientinterface.java

--- src/main/java/com/example/demo/controller/FeignClientinterface.java
+++ src/main/java/com/example/demo/controller/FeignClientinterface.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "Service-B", url = "http://localhost:8092")
+@FeignClient(name = "Service-B", url = "http://54.156.62.244:8092")
 public interface FeignClientinterface {
     @GetMapping("/api/resource")
     String callSerivceB();
