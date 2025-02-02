65f5f0347c98ad28d9e3a905edaa5feb2eb9e970
veerababu558 <44125493+veerababu558@users.noreply.github.com>
2025-02-02 17:11:51 -0500

    Update ServiceA.java

--- src/main/java/com/example/demo/controller/ServiceA.java
+++ src/main/java/com/example/demo/controller/ServiceA.java
@@ -2,9 +2,11 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ServiceA")
 public class ServiceA {
 	
 	@Autowired
